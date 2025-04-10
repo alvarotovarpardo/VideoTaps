38445e9e9e600ae8c521460b088665736f7b7135
Alvaro Tovar <atovar@sensia-solutions.com>
2025-04-10 08:27:17 +0200

    Merge E, M in same loop

--- src/main.cpp
+++ src/main.cpp
@@ -72,7 +72,7 @@ void applyTap(uchar* input, uchar* output, int rows, int cols, const string& tap
     uchar* buffer = new uchar[rows * cols];
     memcpy(buffer, input, rows * cols);
     
-    if(L == 'E'){
+    if(L != '\0'){
         //uchar* buffer = new uchar[rows * cols];
         //memset(buffer, 0, rows * cols);
         std::ofstream ofile("E2.txt");
@@ -81,52 +81,35 @@ void applyTap(uchar* input, uchar* output, int rows, int cols, const string& tap
                 for(int j = 0; j < tapsNumber; j++){
                     for(int t = 0; t < T; t++){
                         int srcIndex = (i * cols) + (r * regionWidth + j * T + t);
-                        if(r >= R/2){
-                            if(R == 2){
-                                int dstIndex = (i * cols) + (regionWidth * (r + 1) - (T * j + t + 1));    
+                        if(L == 'E'){
+                            if(r >= R/2){
+                                if(R == 2){
+                                    int dstIndex = (i * cols) + (regionWidth * (r + 1) - (T * j + t + 1));    
+                                    buffer[dstIndex] = input[srcIndex];
+                                } else {
+                                    int dstIndex = (i * cols) + (regionWidth * (r + 1) - (T * (j + 1) - t));
+                                    buffer[dstIndex] = input[srcIndex];                                
+                                    ofile << srcIndex << " "<< dstIndex << "\n";// \t||\t " << r << " " << j << " " << t << " \n"; // << static_cast<int>(input[srcIndex]) << std::endl;
+                                }
+                            } else {
+                                int dstIndex = (i * cols) + r * regionWidth + j * T + t;
+                                buffer[dstIndex] = input[srcIndex];
+                                ofile << srcIndex << " "<< dstIndex << "\n"; // \t||\t " << r << " " << j <<  " " << t << " \n"; // << static_cast<int>(input[srcIndex]) << std::endl;
+                            }
+                        } else if (L == 'M'){
+                            if(r >= R/2){
+                                int dstIndex = (i * cols) + r * regionWidth + j * T + t;
                                 buffer[dstIndex] = input[srcIndex];
                             } else {
-                                int dstIndex = (i * cols) + (regionWidth * (r + 1) - (T * (j + 1) - t));
-                                buffer[dstIndex] = input[srcIndex];                                
-                                ofile << srcIndex << " "<< dstIndex << "\n";// \t||\t " << r << " " << j << " " << t << " \n"; // << static_cast<int>(input[srcIndex]) << std::endl;
+                                int dstIndex = (i * cols) + (regionWidth - T * (j + 1) + t);
+                                buffer[dstIndex] = input[srcIndex];
+
                             }
-                        } else {
-                            int dstIndex = (i * cols) + r * regionWidth + j * T + t;
-                            buffer[dstIndex] = input[srcIndex];
-                            ofile << srcIndex << " "<< dstIndex << "\n"; // \t||\t " << r << " " << j <<  " " << t << " \n"; // << static_cast<int>(input[srcIndex]) << std::endl;
+
                         }
                     }
                 }
             } ofile.close();
-        }
-    L = '\0';
-    //memcpy(input, buffer, rows * cols);
-    //delete [] buffer; buffer = nullptr;
-    }
-
-    if(L == 'M'){
-        //uchar* buffer = new uchar[rows * cols];
-        //memset(buffer, 0, rows * cols);
-        if(T == 1 || T == 2){
-            std::ofstream ofile("dstIdx.txt");
-            for(int i = 0; i < regionHeight; i++){
-                for(int r = 0; r < R; r++){
-                    for(int j = 0; j < tapsNumber; j++){
-                        for(int t = 0; t < T; t++){
-                            int srcIndex = (i * cols) + (r * regionWidth + j * T + t);                            
-                            if(r == 0){
-                                int dstIndex = (i * cols) + (regionWidth - T * (j + 1) + t);
-                                ofile << srcIndex << " "<< dstIndex << " \t||\t " << r << " " << j << " " <<  static_cast<int>(input[srcIndex]) << std::endl;
-                                buffer[dstIndex] = input[srcIndex];
-                            } else if(r >= R/2){
-                                int dstIndex = (i * cols) + (r * regionWidth + j * T + t);
-                                buffer[dstIndex] = input[srcIndex];
-                                ofile << srcIndex << " "<< dstIndex << " \t||\t " << r << " " << j << " " <<  static_cast<int>(input[srcIndex]) << std::endl;
-                            }
-                        }
-                    }
-                } ofile.close();
-            }
         }
     L = '\0';
     //memcpy(input, buffer, rows * cols);
